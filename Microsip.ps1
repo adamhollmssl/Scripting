28d67937d57c374f32d39b41972b1ff08c98eacb
adamhollmssl <65178225+adamhollmssl@users.noreply.github.com>
2020-07-24 15:16:15 +0100

    Add files via upload

---
+++ Microsip.ps1
@@ -0,0 +1,8 @@
+﻿Start-Process -FilePath "C:\Temp\Microsip.exe" -ArgumentList "/S /D=C:\Microsip"
+
+$TargetFile = "C:\Microsip\Microsip.exe"
+$ShortcutFile = "$env:Public\Desktop\Microsip.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
