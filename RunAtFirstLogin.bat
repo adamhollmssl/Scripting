6b258a5d41f72994e1075d54b72763e498f34979
adamhollmssl <65178225+adamhollmssl@users.noreply.github.com>
2021-02-04 11:16:03 +0000

    Update RunAtFirstLogin.bat

--- RunAtFirstLogin.bat
+++ RunAtFirstLogin.bat
@@ -1,4 +1,6 @@
 powershell.exe set-executionpolicy Unrestricted
+cd C:\
+mkdir Temp
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/adamhollmssl/Scripting/master/GenericAdminScript-Desktops.ps1 -OutFile C:\temp\GenericAdminScript-Desktops.ps1"
 powershell.exe "C:\temp\GenericAdminScript-Desktops.ps1"
 powershell.exe set-executionpolicy Restricted
